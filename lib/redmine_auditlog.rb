363d954d713bfbd6c303a9eb513ad75a6e78ca7e
Alex Stanev <alex@stanev.org>
2018-05-25 15:59:53 +0300

    Patch every module we need, so we can avoid crashes with inherited models. While here, filter tokens for new sessions, salts and password hashes for users.

--- lib/redmine_auditlog.rb
+++ lib/redmine_auditlog.rb
@@ -3,7 +3,26 @@ module RedmineAuditlog
   module AuditlogPatch
     def self.included(base)
       base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
         audited 
+      end
+    end
+  end
+
+  module AuditlogPatchUser
+    def self.included(base)
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+        audited except: [:salt, :hashed_password]
+      end
+    end
+  end
+
+  module AuditlogPatchToken
+    def self.included(base)
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+        audited except: :value
       end
     end
   end

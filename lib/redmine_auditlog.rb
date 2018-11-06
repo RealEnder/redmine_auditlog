669636efd36648e99ac9766cd96bf6de4d781a3f
Alex Stanev <alex@stanev.org>
2018-11-06 15:23:16 +0200

    Cleanup trailing space

--- lib/redmine_auditlog.rb
+++ lib/redmine_auditlog.rb
@@ -4,7 +4,7 @@ module RedmineAuditlog
     def self.included(base)
       base.class_eval do
         unloadable # Send unloadable so it will not be unloaded in development
-        audited 
+        audited
       end
     end
   end
